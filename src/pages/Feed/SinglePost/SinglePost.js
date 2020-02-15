fab06b721d42d3f4d58ba5fde2437c1ad17d0167
Asel <peirisasel373@gmail.com>
2020-02-15 09:43:55 +0530

    Changed Feed fetch link

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -29,7 +29,8 @@ class SinglePost extends Component {
         this.setState({
           title: resData.post.title,
           author: resData.post.creator.name,
-          image: "http://localhost:8080/" + resData.post.imageUrl,
+          image:
+            "https://safe-dusk-64983.herokuapp.com/" + resData.post.imageUrl,
           date: new Date(resData.post.createdAt).toLocaleDateString("en-US"),
           content: resData.post.content
         });
