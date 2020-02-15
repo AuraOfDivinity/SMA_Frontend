4ca1a746ffc104b196e0406cfda454624fd5475f
Asel <peirisasel373@gmail.com>
2020-02-15 09:42:41 +0530

    Changed Feed fetch link

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -14,7 +14,7 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch("http://localhost:8080/feed/post/" + postId, {
+    fetch("https://safe-dusk-64983.herokuapp.com/feed/post/" + postId, {
       headers: {
         Authorization: "Bearer " + this.props.token
       }
